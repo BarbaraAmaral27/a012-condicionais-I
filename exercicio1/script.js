f186215eb4018cd35fe834ea8ea9652049f7d73d
BarbaraAmaral27 <barbaramamaral@gmail.com>
2022-09-07 01:43:27 -0300

    exercicios usando condicionais |

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,15 @@
-const num = //seu numero aqui
+const num = prompt("Digite um número:")
 
-if(){
-    console.log("sua mensagem aqui");
+if(num%2===0){
+    console.log("O número digitado é par!")
 }
+else if (num%2===1){
+    console.log("O número digitado é ímpar!")
+}
+
+
+// else
+//     console.log("O número digitado é ímpar!")
+        
+
+
